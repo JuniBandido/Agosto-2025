cb0210baa2d71962e80a3028611134732147d6d0
José Noguera <juniornoguera1@gmail.com>
2025-08-07 17:44:17 -0600

    avance

--- Actividad-16.py
+++ Actividad-16.py
@@ -1,22 +1,40 @@
+from tkinter.messagebox import showinfo
+
+
 class Libro:
     def __init__(self, tittle, author, publiYear):
         self.tittle = tittle
         self.author = author
         self.publiYear = publiYear
 
+    def showInfo(self):
+        print(f"Titulo: {self.tittle}\nAutor: {self.author}\nAño de publicacion: {self.publiYear}")
+
 books = []
 
-print("1. Agregar libro")
-print("2. Mostrar lista de libros")
-print("3. Eliminar libro por título")
-print("4. Salir del programa")
-menu = input("Seleccione una opcion: ")
+while True:
+    print("1. Agregar libro")
+    print("2. Mostrar lista de libros")
+    print("3. Eliminar libro por título")
+    print("4. Salir del programa")
+    menu = input("Seleccione una opcion: ")
 
-titulo_libro = input("Ingrese el titulo del libro: ")
-autor_libro = input("Ingrese el autor del libro: ")
-publicacion_libro = input("Ingrese el año de publicación: ")
+    match menu:
+        case "1":
+            titulo_libro = input("Ingrese el titulo del libro: ")
+            autor_libro = input("Ingrese el autor del libro: ")
+            publicacion_libro = input("Ingrese el año de publicación: ")
+            print()
 
-book1 = Libro(titulo_libro, autor_libro, publicacion_libro)
+            book1 = Libro(titulo_libro, autor_libro, publicacion_libro)
 
-books.append(book1)
+            books.append(book1)
 
+        case "2":
+            count = 0
+            for i in books:
+                print(f"Titulo: {books[count].tittle}")
+                print(f"Autor: {books[count].author}")
+                print(f"Año de publicación: {books[count].publiYear}")
+                print()
+                count += 1
