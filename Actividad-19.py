a4b9af6bd1bfa364edd75194062bd5a657b8a360
José Noguera <juniornoguera1@gmail.com>
2025-08-13 21:32:24 -0600

    Avance Programa actividad 19

--- Actividad-19.py
+++ Actividad-19.py
@@ -34,6 +34,25 @@ class GalletaRellena(Galletas, Relleno):
     def mostrar_galletaRellena(self):
         return f"{Galletas.mostrar_info(self)}, {Relleno.sabor_relleno(self)} y {Relleno.describir_relleno(self)}"
 
+class GalletaRellenaGuardada(Galletas, Relleno):
+    def __init__(self, name, price, weight, sabor, descripcion):
+        Galletas.__init__(self, name, price, weight)
+        self.sabor = sabor
+        self.descripcion = descripcion
+
+    def mostrar_info(self):
+        print(f"Nombre Galleta: {self.name} | Precio: {self.price} | Peso: {self.weight} | Relleno: {self.sabor} ({self.descripcion})")
+
+class RegistrarRellena(GalletaRellena):
+    def registrar_galleta_rellena(self):
+        name = input("Ingresa el nombre: ")
+        price = int(input("Ingresa el precio: "))
+        weight = input("Ingresa el peso: ")
+        sabor = input("Ingresa el sabor del relleno: ")
+        descripcion = input("Describe el relleno: ")
+        print()
+        listCoockies.append(GalletaRellenaGuardada(name, price, weight, sabor, descripcion))
+
 class RegistrarBasica(Galletas):
     def registrar_galleta_basica(self):
         name = input("Ingresa el nombre: ")
@@ -41,6 +60,24 @@ class RegistrarBasica(Galletas):
         weight = input("Ingresa el peso: ")
         print()
         listCoockies.append(Galletas(name, price, weight))
+
+class GalletaChispasGuardada(Galletas):
+    def __init__(self, name, price, weight, chispas):
+        super().__init__(name, price, weight)
+        self.chispas = chispas
+
+    def mostrar_info(self):
+        print(f"Nombre Galleta: {self.name} | Precio: {self.price} | Peso: {self.weight} | Chispas: {self.chispas}")
+
+class RegistrarChispas(GalletaChispas):
+    def registrar_galleta_chispas(self):
+        name = input("Ingresa el nombre: ")
+        price = int(input("Ingresa el precio: "))
+        weight = input("Ingresa el peso: ")
+        chispas = int(input("Ingresa la cantidad de chispas: "))
+        print()
+        listCoockies.append(GalletaChispasGuardada(name, price, weight, chispas))
+
 
 def mostrar_galletas():
     if not listCoockies:
@@ -75,11 +112,14 @@ def eliminar_nombre():
     print()
 
 while True:
-    menu = input("1. Agregar galleta\n4. Mostrar Lista\n5. Buscar por nombre\n6. Eliminar\n7. Salir\nEscoja una opcion: ")
+    menu = input("1. Agregar galleta\n2. Agregar galleta con chispas\n3. Agregar galleta con relleno\n4. Mostrar Lista\n5. Buscar por nombre\n6. Eliminar\n7. Salir\nEscoja una opcion: ")
     print()
     match menu:
         case "1":
             RegistrarBasica.registrar_galleta_basica(Galletas)
+
+        case "2":
+            RegistrarChispas.registrar_galleta_chispas(Galletas)
 
         case "4":
             mostrar_galletas()
